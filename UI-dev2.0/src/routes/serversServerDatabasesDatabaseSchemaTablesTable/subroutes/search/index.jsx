7360c711167ee4a0d235a9ea55b107f480247d16
Itay Moav <itay.malimovka@gmail.com>
2023-01-29 16:10:21 -0500

    search page - done

--- UI-dev2.0/src/routes/serversServerDatabasesDatabaseSchemaTablesTable/subroutes/search/index.jsx
+++ UI-dev2.0/src/routes/serversServerDatabasesDatabaseSchemaTablesTable/subroutes/search/index.jsx
@@ -33,34 +33,59 @@ export default function TableSearch(){
         <>
         <Jumbotron>
         <form onSubmit={handleSubmit(onSubmit)}>
-        <Table striped bordered hover size="sm" variant="dark">
-            <thead>
-                <tr key="2">
-                    <th>Field Name</th>
-                    <th>Type</th>
-                    <th>Constraints</th>
-                    <th>operator</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                {structure.columns.map(tr=>{return (
-                <tr key={tr.column_name}>
-                    <td>{tr.column_name}</td>
-                    <td>{tr.data_type}</td>
-                    <td>{tr.is_primary_key === "1" ? "PK ,":""} {tr.is_nullable === "0"? "Not Null":""}</td>
-                    <td>OPERATOR DROPDOWN</td>
-                    <td><div className="input-group">
-                        <input className="form-control" type="text" {...register(tr.column_name)} />
-                    </div></td>
-                </tr>
-                )})}
-            </tbody>
-        </Table>
-        <div className="text-right">
-            <Button variant="link" onClick={()=>reset()}>clear</Button>
-            <Button variant="primary" type="submit" className="mt-1" tabIndex="0">SEARCH</Button>
-        </div>
+            <div className="text-secondary">
+                Examples:<br />To use operator NOT IN (v,v,...) enter values like this: <code>v1,v2,v3</code>.
+                If those values are strings, enter them like this: <code>'v1','v2','v3'</code>
+            </div>
+            <Table striped bordered hover size="sm" variant="dark">
+                <thead>
+                    <tr key="2">
+                        <th>Field Name</th>
+                        <th>Type</th>
+                        <th>Constraints</th>
+                        <th>Operator</th>
+                        <th>Value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {structure.columns.map(tr=>{return (
+                    <tr key={tr.column_name}>
+                        <td>{tr.column_name}</td>
+                        <td>{tr.data_type}</td>
+                        <td>{tr.is_primary_key === "1" ? "PK ,":""} {tr.is_nullable === "0"? "Not Null":""}</td>
+                        <td>
+                        <div className="input-group">
+                        <select className="form-control" {...register(`operator-${tr.column_name}`)}>
+                            <option value="eq">=</option>
+                            <option value="gt">&gt;</option>
+                            <option value="gteq">&gt;=</option>
+                            <option value="lt">&lt;</option>
+                            <option value="lteq">&lt;=</option>
+                            <option value="noteq">!=</option>
+                            <option value="LIKE">LIKE</option>
+                            <option value="NOTLIKE">NOT LIKE</option>
+                            <option value="LIKEpp">LIKE %...%</option>
+                            <option value="NOTLIKEpp">NOT LIKE %...%</option>
+                            <option value="IN">IN (v,v,...)</option>
+                            <option value="NOTIN">NOT IN (v,v,...)</option>
+                            <option value="BETWEEN">BETWEEN v,v</option>
+                            <option value="NOTBETWEEN">NOT BETWEEN v,v</option>
+                            <option value="ISNULL">IS NULL</option>
+                            <option value="ISNOTNULL">IS NOT NULL</option>
+                        </select>
+                        </div>
+                        </td>
+                        <td><div className="input-group">
+                            <input className="form-control" type="text" {...register(`value-${tr.column_name}`)} />
+                        </div></td>
+                    </tr>
+                    )})}
+                </tbody>
+            </Table>
+            <div className="text-right">
+                <Button variant="link" onClick={()=>reset()}>clear</Button>
+                <Button variant="primary" type="submit" className="mt-1" tabIndex="0">SEARCH</Button>
+            </div>
         </form>
         </Jumbotron>
 
--- UI-dev2.0/src/routes/serversServerDatabasesDatabaseSchemaTablesTable/subroutes/search/useRunSearch.js
+++ UI-dev2.0/src/routes/serversServerDatabasesDatabaseSchemaTablesTable/subroutes/search/useRunSearch.js
@@ -5,23 +5,53 @@ export default function useRunSearch(dispatch,connectionName,server,database,sch
 
     return data=>{
         console.log('SEARCHING WITH',data);
-        let query = `SELECT * FROM ${schema}.${table}`;
-        let where = "";
-        let operator = "=";
-        let AND="";
-
-        const columns = Object.keys(data);
-        for(let i=0;i<columns.length;i++){
-            if(data[columns[i]].length>0){
-                where += ` ${AND} ${columns[i]} ${operator} '${data[columns[i]]}' `;
-                AND = " AND ";
-            }
-        }
-        if(where.length>0){
-            query = `${query} WHERE ${where}`;
-        }
-
+        const where = translateDataToValueOperatorPairs(data);
+        const query = `SELECT * FROM ${schema}.${table} ${where}`;
         const payload={ connectionName,server,database,query};
         dispatch(runQuery(payload));
     }
+}
+
+
+function translateDataToValueOperatorPairs(rawData){
+    let sql="";
+    let sqlAnd = "";
+    const columns = Object.keys(rawData);
+    for(let i=0;i<columns.length;i++){
+        if(columns[i].includes('value-')){
+            const operatorName = rawData[columns[i].replace('value-','operator-')];
+                
+            if(rawData[columns[i]].length>0 || operatorName === 'ISNULL' || operatorName === 'ISNOTNULL'){
+                console.log('Calling operatios',operatorName);
+                const columnName = columns[i].replace('value-','');
+                const sqlCondition = Operators[operatorName](rawData[columns[i]]);
+                sql += ` ${sqlAnd} ${columnName} ${sqlCondition} `;
+                sqlAnd = " AND ";
+            }
+        }
+    }
+
+    if(sql.length>0){
+        sql = ` WHERE ${sql}`;
+    }
+    return sql;
+}
+
+const Operators = {
+    eq: value => ` ='${value}'`,
+    gt: value => ` >'${value}'`,
+    gteq: value => ` >='${value}'`,
+    lt: value => ` <'${value}'`,
+    lteq: value => ` <='${value}'`,
+    noteq: value => ` <>'${value}'`,
+    LIKE: value => ` LIKE '${value}'`,
+    NOTLIKE: value => `NOT LIKE ='${value}'`,
+    LIKEpp: value => ` LIKE '%${value}%'`,
+    NOTLIKEpp: value => `NOT LIKE ='%${value}%'`,
+    IN: value => ` IN (${value})`,
+    NOTIN: value => ` NOT IN(${value})`,
+    BETWEEN: value => ` BETWEEN '${value.replace(",","' AND '")}'`,
+    NOTBETWEEN: value => ` NOT BETWEEN '${value.replace(",","' AND '")}'`,
+    ISNULL: () => ` IS NULL`,
+    ISNOTNULL: () => ` IS NOT NULL`
 }
