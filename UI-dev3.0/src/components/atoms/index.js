ce0c5db954bcaa9e94e26dcc97379ae7493243a1
Itay Moav <itay.malimovka@gmail.com>
2023-11-24 11:35:21 -0500

    favicons

--- UI-dev3.0/src/components/atoms/index.js
+++ UI-dev3.0/src/components/atoms/index.js
@@ -3,12 +3,10 @@ import PageTitle from "./pageTitle";
 import OperationCard from "./operationCard";
 import FormFieldError from "./formFieldError";
 
-///*TOBEDELETED20230128  import TableWithActions from "./tableWithActions";
 import "./atoms.css";
 export {
     Jumbotron,
     PageTitle,
     OperationCard,
-    FormFieldError//,
-    ///*TOBEDELETED20230128    TableWithActions
+    FormFieldError
 };
