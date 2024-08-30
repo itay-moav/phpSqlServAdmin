3cbb41b292c65a3eac9356a1e620138a5e0e268e
Itay Moav <itay.malimovka@gmail.com>
2024-08-29 21:18:25 -0400

    starting v3.1.0

--- UI-dev3.1.0/vite.config.js
+++ UI-dev3.1.0/vite.config.js
@@ -3,13 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    fs: {
+      // Allow serving files from one level up to the project root
+      allow: ['..'],//need to allow the font files from node modules
+    },
+  },
   build: {
       target: 'esnext'
   },
   base: "/vulcan",
   plugins: [react()],
   define: {
-      __APP_VERSION__: JSON.stringify('3.0.0'),
+      __APP_VERSION__: JSON.stringify('3.1.0'),
       __APP_NAME__:    JSON.stringify(process.env.npm_package_name)
   }
 })
